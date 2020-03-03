2e06270306435c0eec3b5ccc7ab6ebf4e398452f
XQ <dahuaciliu@gmail.com>
2020-03-03 17:50:03 +0800

    add pointer

---
+++ pointer/wallet_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+type Wallet struct {
+	balance int
+}
+
+func (w *Wallet) Deposit(amount int) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() int {
+	return w.balance
+}
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	want := 10
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
