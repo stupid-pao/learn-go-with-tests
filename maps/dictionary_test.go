4bec5fba6b94a54864ea9d0485925c0056ef0bb5
XQ <dahuaciliu@gmail.com>
2020-03-08 22:17:29 +0800

    map --todo: Add() should not cover exist key

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -17,6 +17,11 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
+func (d Dictionary) Add(word, definition string) {
+	// map 又一个有趣的特性，不实用指针就可以修改他们。这是因为 map 是饮用类型。
+	d[word] = definition
+}
+
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
@@ -49,3 +54,35 @@ func assertStrings(t *testing.T, got, want string) {
 		t.Errorf("got '%s' want '%s' given", got, want)
 	}
 }
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+	dictionary.Add(word, definition)
+
+	assertDefinition(t, dictionary, word, definition)
+
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word", err)
+	}
+
+	if definition != got {
+		t.Errorf("got '%s' want '%s'", got, definition)
+	}
+
+}
+
+/*
+map 作为引用类型很好，因为无论map有多大，都只会有一个副本。
+但是尝试使用一个nil的map会得到指针异常，
+所以永远不要初始化一个空的 map 像这样： var m map[string]string
+正确使用方式：
+	dictionary := map[string]string{}
+	or
+	dictionary := make[map[string]string]
+*/
