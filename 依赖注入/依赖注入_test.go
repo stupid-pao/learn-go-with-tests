666a6222c8bbd8faddf6617ac9726b386e255f86
XQ <dahuaciliu@gmail.com>
2020-03-10 16:31:45 +0800

    little modify

--- 依赖注入/依赖注入_test.go
+++ 依赖注入/依赖注入_test.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"bytes"
-	"fmt"
-	"io"
 	"testing"
 )
 
-func Greet(writer io.Writer, name string) {
-	fmt.Fprintf(writer, "Hello, %s", name)
-}
+//func Greet(writer io.Writer, name string) {
+//fmt.Fprintf(writer, "Hello, %s", name)
+//}
 
 func TestGreet(t *testing.T) {
 	buffer := bytes.Buffer{}
