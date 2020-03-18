a0354080438fd4e3e5697f065c5844184816ec04
XQ <dahuaciliu@gmail.com>
2020-03-18 17:57:27 +0800

    concurrency

---
+++ concurrency/web_test.go
@@ -0,0 +1,39 @@
+package concurrency
+
+import (
+	"reflect"
+	"testing"
+)
+
+func mockWebsiteChecker(url string) bool {
+	if url == "waat://furhurterwe.geds" {
+		return false
+	}
+	return true
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"waat://furhurterwe.geds",
+		"http://blog.gypsydave5.com",
+	}
+
+	actualResults := CheckWebsites(mockWebsiteChecker, websites)
+
+	want := len(websites)
+	got := len(actualResults)
+	if want != got {
+		t.Fatalf("want %v, got %v", want, got)
+	}
+
+	expecteResults := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	if !reflect.DeepEqual(expecteResults, actualResults) {
+		t.Fatalf("Wanted %v, got %v", expecteResults, actualResults)
+	}
+}
