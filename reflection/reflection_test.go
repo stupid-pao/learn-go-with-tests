534804cfda3ab538bce20c0556e23e3d71cac72a
XQ <dahuaciliu@gmail.com>
2020-03-21 22:56:00 +0800

    map 无序 重新增加新的测试用例

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -68,14 +68,6 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Reykjavik"},
 		},
-		{
-			"Map",
-			map[string]string{
-				"Foo": "Bar",
-				"Baz": "Boz",
-			},
-			[]string{"Bar", "Boz"},
-		},
 	}
 
 	for _, test := range cases {
@@ -91,4 +83,32 @@ func TestWalk(t *testing.T) {
 		})
 	}
 
+	t.Run("with maps", func(t *testing.T) {
+
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+
+}
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Fatalf("expected %+v to contain '%s' but it didn't", haystack, needle)
+	}
 }
